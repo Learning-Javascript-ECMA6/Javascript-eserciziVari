2511b2d0724adce8c5d2b05f964c719255ed18ae
valentina <valentinagiannone@gmail.com>
2019-10-31 16:54:27 +0100

    stampa numeri da 1000 a 0 e viceversa - finito

---
+++ lib9/js/script.js
@@ -0,0 +1,18 @@
+
+//stampa tutti i numeri da 1000 a 0
+
+for(var i = 1000 ; i > 0; i-- ){
+ console.log(i);
+ document.writeln(i);
+}
+
+
+
+
+
+//Stampa tutti i numeri da 0 a 1000
+
+for(var i = 0 ; i <= 1000; i++ ){
+ console.log(i);
+ document.writeln(i);
+}
