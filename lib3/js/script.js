fc0636277a707c579efcdbae28e85adb4ecdace0
valentina <valentinagiannone@gmail.com>
2019-10-31 15:06:21 +0100

    stampare la parola più lunga-finito-

---
+++ lib3/js/script.js
@@ -0,0 +1,23 @@
+//Inserisci due parole, quale è la piu lunga?
+
+
+//inserisco la prima parola data dall'utente
+var parola1 = prompt("inserisci una parola");
+console.log("la 1 parola inserita sarà " + parola1);
+
+//inserisco la seconda parola data dall'utente
+var parola2 = prompt("inserisci un altra parola");
+console.log("la 2 parola inserita sarà " + parola2);
+
+//utilizzo le condizioni per stabilirne la lunghezza
+if (parola1>parola2) {
+ //se parola1 è maggiore di parola2
+ console.log("la parola piu lunga è " + parola1);
+ //la parola più lunga è parola1
+ document.writeln ("La parola piu lunga è " + parola1 + " .");
+}
+//viceversa è parola2
+else{
+ console.log("la parola piu lunga è " + parola2);
+ document.writeln ("La parola piu lunga è : " + parola2 + " .");
+};
