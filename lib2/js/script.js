645fd8a7c40c516ce828bd96bdb21dca4ec4c86c
valentina <valentinagiannone@gmail.com>
2019-10-31 14:50:53 +0100

    finito

--- lib2/js/script.js
+++ lib2/js/script.js
@@ -4,18 +4,18 @@
 //Hai creato un (insicurissimo) password generator!
 
 //Chiedo all’utente il suo nome
-var nome = prompt("come ti chiami?")
+var nome = prompt("Ciao, come ti chiami?")
  console.log("il nome utente sarà : " + nome );
 
 //Chiedo all’utente il suo cognome
-var cognome = prompt("qual è il tuo cognome?")
+var cognome = prompt("Qual è il tuo cognome?")
  console.log("il cognome utente sarà : " + cognome );
 
 //Chiedo all’utente il suo colore preferito
-var colore = prompt("qual è il tuo colore preferito?")
+var colore = prompt("Qual è il tuo colore preferito?")
  console.log("il colore preferito sarà : " + colore );
 
-//genero la password sommando nome cognome colorepreferito e aggiungo n 
+//genero la password sommando nome cognome colorepreferito e aggiungo n
  risposta = (nome + cognome + colore + 14);
  console.log("allora la password utente sarà : " + risposta);
- document.writeln("ciao " + nome +  " la tua nuova password è : " + risposta);
+ document.writeln("Ciao " + nome +  ", la tua nuova password è : " + risposta + ".");
