9214ed81f3f3edec092b0fb967beabd8de8f52de
valentina <valentinagiannone@gmail.com>
2019-10-31 16:37:30 +0100

    stampa il numero successivo-finito

---
+++ lib7/js/script.js
@@ -0,0 +1,8 @@
+//Chiedi un numero all’utente e stampa in console il numero successivo.
+
+var numero = parseInt(prompt("scegli un numero"))
+console.log("il numero utente sarà : " + numero++);
+
+risultato = numero++
+console.log("Il risultato sarà " + risultato);
+document.writeln("Il risultato sarà " + risultato, ".")
