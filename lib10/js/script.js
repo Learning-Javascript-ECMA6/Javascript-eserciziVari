f0b9e62f9806ac0a05f0b2d043e5e68213bf2b97
valentina <valentinagiannone@gmail.com>
2019-11-01 14:46:18 +0100

    crea un array di nomi e stampali/ crea un array di numeri e stampa solo dispari - finito

--- lib10/js/script.js
+++ lib10/js/script.js
@@ -4,6 +4,17 @@ var list =["Vale, ", "Silvia, ","Sabrina, ","Luisa, ","Marco."];
 console.log(list);
 
 for (var i = 0; i < list.length; i++){
-console.log ("I nomi contenuti nell'array sono : " , list[i], " .");
-document.writeln (list[i]);
+console.log (list[i]);
+document.writeln(list[i]);
+}
+
+
+//Crea un array di numeri, e stampa solo i numeri dispari
+var number = [1,2,3,4,5,6,7,8,9,10];
+
+for (var i = 0; i < number.length; i++){
+ if(number[i] % 2 !== 0){
+  console.log(number[i]);
+   document.writeln (number[i]);
+ }
 }
