365b7cd9b7cb3f883d6c19777732387569758923
valentina <valentinagiannone@gmail.com>
2019-11-01 14:24:24 +0100

    crea un array di nomi e stampali - finito

---
+++ lib10/js/script.js
@@ -0,0 +1,9 @@
+//Crea un array contenente 5 nomi e stampali a schermo
+
+var list =["Vale, ", "Silvia, ","Sabrina, ","Luisa, ","Marco."];
+console.log(list);
+
+for (var i = 0; i < list.length; i++){
+console.log ("I nomi contenuti nell'array sono : " , list[i], " .");
+document.writeln (list[i]);
+}
