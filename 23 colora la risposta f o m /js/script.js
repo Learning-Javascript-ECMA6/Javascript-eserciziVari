96641c34fa6b56b66e0962c7f448aa3212025f23
valentina <valentinagiannone@gmail.com>
2020-03-18 21:06:48 +0100

    new ex

---
+++ 23 colora la risposta f o m /js/script.js
@@ -0,0 +1,18 @@
+/* Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+Sulla pagina html deve apparire “Ciao < nome >”, il colore del nome deve essere azzurro o
+rosa a seconda del sesso inserito */
+/*
+var nome =prompt('come ti chiami?');
+var sesso = prompt('sei maschio o femmina?');
+var element = document.getElementById('id');
+
+if (sesso == 'maschio') {
+    console.log('blu');
+    element.innerHTML = 'ciao ' + nome , '.';
+    element.style.color = 'blue';
+} else if (sesso == 'femmina'){
+    element.innerHTML = 'ciao ' + nome , '.';
+    element.style.color = 'pink';
+    console.log('rosa');
+}
+ */
